8e2a868c2e2220612ae7e6a47a89aa92ead630e8
DavidGTeklea <87445117+DavidGTeklea@users.noreply.github.com>
2024-04-02 16:15:37 -0400

    Update InstanceType.java

--- src/main/java/edu/pitt/cs/InstanceType.java
+++ src/main/java/edu/pitt/cs/InstanceType.java
@@ -1,5 +1,93 @@
 package edu.pitt.cs;
 
-public enum InstanceType {
-	IMPL, BUGGY, SOLUTION, MOCK
+public class CatImpl implements Cat {
+
+	// TODO: Fill in with member variables
+	int id;
+	String name;
+	boolean isRented;
+
+	public CatImpl(int id, String name) {
+		// TODO: Fill in
+		this.id = id;
+		this.name = name;
+		isRented = false;
+	}
+
+	/**
+	 * Rent cat. Simply sets the _rented flag to true.
+	 */
+	public void rentCat() {
+		// TODO: Fill in
+		isRented = true;
+	}
+
+	/**
+	 * Return cat. Simply sets the _rented flag to false.
+	 */
+	public void returnCat() {
+		// TODO: Fill in
+		isRented = false;
+	}
+
+	/**
+	 * Rename cat. Simply sets the name to the new name.
+	 * 
+	 * @param String name New name of the cat
+	 */
+	public void renameCat(String name) {
+		// TODO: Fill in
+		this.name = name;
+	}
+
+	/**
+	 * Accessor for _name variable. Returns the name of this cat.
+	 * 
+	 * @return String name of cat
+	 */
+
+	public String getName() {
+		// TODO: Fill in
+		return name;
+	}
+
+	/**
+	 * Accessor for _id variable. Returns the ID of this cat.
+	 * 
+	 * @return int ID of this cat
+	 */
+
+	public int getId() {
+		// TODO: Fill in
+		return id;
+	}
+
+	/**
+	 * Accessor for _rented variable. Returns if cat is rented.
+	 * 
+	 * @return boolean - true if rented, false otherwise
+	 */
+
+	public boolean getRented() {
+		// TODO: Fill in
+		if (isRented) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
+	/**
+	 * Returns string version of this cat, in form: "ID *id_num*. *name*" Example
+	 * for cat of ID 1, name Jennyanydots: "ID 1. Jennyanydots"
+	 * 
+	 * @return String string version of cat
+	 */
+
+	public String toString() {
+		// TODO: Fill in
+		return "ID " + id + ". " + name;
+	}
+
 }
